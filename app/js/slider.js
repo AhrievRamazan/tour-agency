cd129534f25d1c413c3a5a15e43f3b5d7f2ca269
AhrievRamazan <ramazanahriev278@gmail.com>
2022-11-11 23:05:59 +0300

    tour-agency

---
+++ app/js/slider.js
@@ -0,0 +1,25 @@
+$(function () {
+  const imageBox = document.querySelectorAll('.slider-box')
+  const imageView = document.querySelector('.image-view')
+  const galleryButton = document.querySelectorAll('.gallery-btn')
+
+
+
+
+
+
+
+
+
+  $('.expert-slider').slick({
+    infinite: false,
+    slidesToShow: 2,
+    slidesToScroll: 1
+  });
+  $('.slider-box').slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+
+})
