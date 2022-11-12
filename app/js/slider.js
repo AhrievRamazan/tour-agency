3a74a70dfc7aa4e3df5d284300c64962c06d6eb9
AhrievRamazan <ramazanahriev278@gmail.com>
2022-11-12 20:39:16 +0300

    final-change

--- app/js/slider.js
+++ app/js/slider.js
@@ -4,17 +4,51 @@ $(function () {
   const galleryButton = document.querySelectorAll('.gallery-btn')
 
 
+  $('.profile-slider').slick({
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    touchThreshold: 15,
+    speed: 500,
+    easing: 'easy',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    
+    ]
+  })
 
-
-  $('.expert-slider').slick({
+  $('.slider-box'), $('.expert-slider').slick({
     infinite: false,
     slidesToShow: 2,
-    slidesToScroll: 1
-  });
-  $('.slider-box').slick({
-    infinite: false,
-    slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    touchThreshold: 15,
+    speed: 500,
+    easing: 'easy',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow:1,
+        }
+      }
+    ]
   });
 
 })
